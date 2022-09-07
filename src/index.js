23e20e0b297e2eca795120ffe3fddd4d1cdea36a
Emmanuel Nwoye <emmanuelnwoye5@gmail.com>
2022-09-07 01:32:02 +0100

    edited index

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
-// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import App from './lib/components/app';
-// import { Provider } from 'react-redux';
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { fetchUsers } from "./lib/api/usersApiSlice";
-// import store from './store';
-
-// store.dispatch(fetchUsers());
-
-// ReactDOM.render(
-//     <Provider store={store}>
-//         <App />
-//     </Provider>
-//     ,
-//     document.getElementById('root')
-// );
-
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { render } from "react-dom";
+import ReactDOM from 'react-dom';
 import App from './lib/components/app';
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <App />
+    ,
+    document.getElementById('root')
+);
